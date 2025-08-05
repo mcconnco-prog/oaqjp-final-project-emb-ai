ca7f2e08535cf63d69c86b3c96359f90979d4b72
mcconnco-prog <mcconnco@gmail.com>
2025-08-05 15:14:43 -0400

    Created emotion detection function

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+import requests, json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, headers = headers, json = myobj)
+    return response.text
