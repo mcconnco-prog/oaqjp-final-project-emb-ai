6411bf6c148671a7360563e7361f5e68006b9b42
mcconnco-prog <mcconnco@gmail.com>
2025-08-05 16:02:33 -0400

    Formatted the response to be json

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,4 +5,28 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers = headers, json = myobj)
-    return response.text
+    json_response = json.loads(response.text) 
+    emotion_data = json_response['emotionPredictions'][0]
+    
+    anger = emotion_data['emotion']['anger']
+    disgust = emotion_data['emotion']['disgust']
+    fear = emotion_data['emotion']['fear']    
+    joy = emotion_data['emotion']['joy']
+    sadness = emotion_data['emotion']['sadness']
+
+    emotions = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy':joy,
+        "sadness": sadness,
+    }
+
+    dominant_emotion = max(emotions, key= emotions.get)
+
+    return {'anger': anger, 
+    'disgust': disgust,
+    'fear': fear,
+    'joy': joy, 
+    'sadness': sadness, 
+    "dominant emotion": dominant_emotion}
